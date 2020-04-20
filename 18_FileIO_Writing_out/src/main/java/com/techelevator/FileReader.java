f764b2cd8b9c3272d4a448a16ec0d336d3d27e9b
Brandon Fannin <bfannin13@gmail.com>
2020-04-20 14:40:50 -0400

    refactored project structure for github upload

--- 18_FileIO_Writing_out/src/main/java/com/techelevator/FileReader.java
+++
@@ -1,9 +0,0 @@
-package com.techelevator;
-
-import java.io.FileNotFoundException;
-import java.util.List;
-
-public interface FileReader {
-
-	List<String> read() throws FileNotFoundException;
-}
--- 18_FileIO_Writing_out/src/main/java/com/techelevator/FileWriter.java
+++
@@ -1,9 +0,0 @@
-package com.techelevator;
-
-import java.io.IOException;
-import java.util.List;
-
-public interface FileWriter {
-
-	void write(List<String> lines) throws IOException;
-}
